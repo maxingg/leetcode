6196f47b8368479b77453ec953ab6bc338f38292
maxingg <1501901576@qq.com>
2020-03-15 20:35:42 +0800

    链表两两交换结点

---
+++ 24.swap-nodes-in-pairs.java
@@ -0,0 +1,28 @@
+/*
+ * @lc app=leetcode id=24 lang=java
+ *
+ * [24] Swap Nodes in Pairs
+ */
+
+// @lc code=start
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode(int x) { val = x; }
+ * }
+ */
+class Solution {
+    //只交换结点，我们考虑一般情况下如何交换结点值的操作
+    public ListNode swapPairs(ListNode head) {
+        if(head == null || head.next == null)
+            return head;
+        ListNode n = head.next;
+        head.next = swapPairs(head.next.next);
+        n.next = head;
+        return n;
+    }
+}
+// @lc code=end
+
